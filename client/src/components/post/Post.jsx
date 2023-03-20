fb24f2b33be8b78aca96fd69fd52f1b804ab2773
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-03-21 00:54:51 +0530

    Post rendering with CRUD

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,38 +1,26 @@
-import { Link } from "react-router-dom";
 import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post({ post }, { img }) {
+export default function Post({ post }) {
+  const PF = "http://localhost:5000/images/";
   return (
     <div className="post">
-      {post.photo && (
-        <img
-        className="postImg"
-        src={post.photo}
-        alt=""
-      />)}
-      
-
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          {
-            post.categories.map((category) =>
-              <span className="postCat">
-                {category.name}
-          </span>)
-          }
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
         <Link to={`/post/${post._id}`} className="link">
-          <span className="postTitle">
-            {post.title}
-          </span>
+          <span className="postTitle">{post.title}</span>
         </Link>
         <hr />
-        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-
-      <p className="postDesc">
-        {post.desc}
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,17 @@
 import { useLocation } from 'react-router'
 import './singlePost.css'
 import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
 
 export default function SinglePost() {
+    const PF = "http://localhost:5000/images/";
     const location = useLocation();
     const path = (location.pathname.split("/")[2]);
+    // console.log(path);
     const [post, setPost] = useState({});
+    const { user } = useContext(Context);
 
     useEffect(() => {
         const getPost = async () => {
@@ -15,39 +19,51 @@ export default function SinglePost() {
             setPost(res.data);
         };
         getPost();
-    },[path]);
+    }, [path]);
+    
+    const handleDelete = async () => { 
+        try {
+             await axios.delete(`/posts/${post._id}`, {
+             data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (err) {}
+    }
 return (
     <div className='singlePost'>
         
         <div className="singlePostWrapper">
             {post.photo && (<img
             className="singlePostImg"
-            src={post.photo}
+            src={PF + post.photo}
             alt=""/> )}
             
             <h1 className="singlePostTitle">
                 {post.title}
-                <div className="singlePostEdit">
-                    <i className="singlePostIcon far fa-edit"></i>
-                    <i className="singlePostIcon far fa-trash-alt"></i>
-                </div>
+                {post.username === user?.username && (
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+                    </div>)
+                }
             </h1>
 
             <div className="singlePostInfo">
                 <span>
                     Author:
                     <Link to={`/?user=${post.username}`}  className="link">
-                        <b className="singlePostAuthor">{post.username}</b>
+                        <b className="singlePostAuthor">
+                            {post.username}
+                        </b>
                     </Link> 
                 </span>
                 <span>1{new Date(post.createdAt).toDateString()}</span>
             </div>
 
             <p className="singlePostDesc">
-            {post.desc}
+                {post.desc}
             </p>
-
         </div>
     </div>
-)
-}
+    )
+};
